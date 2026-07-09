f8697edbb372dad783b24da61aef3dd24c0559b2
Aman <aman8122002@gmail.com>
2026-07-09 12:06:07 +0530

    Corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Register from "./components/Register";
-import Contact from "./components/Contact";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
